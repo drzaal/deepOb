29d8bac94047250e7be956ebae47fdc15cae07a6
Zaal Tonia <drzaal@gmail.com>
2016-06-04 16:15:46 -0700

    Cannot expose a getter/setter as the result of the deepOb method

--- index.js
+++ index.js
@@ -4,85 +4,87 @@ function isString(ob) {
 	return Object.prototype.toString.call(ob) == '[object String]';
 }
 
-Object.defineProperty(Object.prototype, "deepSet", {
+Object.defineProperty(Object.prototype, "deepOb", {
+	configurable: true,
 	enumerable: false,
-	value: function (path, value) {
-		function isArray(ob) {
-			return Object.prototype.toString.call(ob) == '[object Array]';
-		}
-		function isObject(ob) {
-			return Object.prototype.toString.call(ob) == '[object Object]';
-		}
-		function isFunction(ob) {
-			return Object.prototype.toString.call(ob) == '[object Function]';
-		}
-
-		if (!isString(path)) { 
-			throw new Error("Invalid path");
-		}
-		var pathArry = path.split('.');
-
-		pathArry.forEach(function(val, key) {
-			if (!val.match(/\w+/)) {
-				throw new Error("Invalid property " + val + " in " + this.toString + " " + pathArry.slice(0,key).join('.'));
-			}
-		});
-		var valueReceptor = pathArry.pop();
-
-		var i,imax, cursor;
-		imax = pathArry.length;
-		cursor = this;
-
-		for (i=0;i<imax;i++) {
-			var cursorName = pathArry[i];
-
-			if (cursor[cursorName] === undefined) {
-				cursor = cursor[cursorName] = {};
-			} else if (isObject(cursor[cursorName]) || isFunction(cursor[cursorName])) {
-				cursor = cursor[cursorName];
-			} else if (isArray(cursor[cursorName])) {
-				cursor = cursor[cursorName] = cursor[cursorName].reduce(function(ob, val, key) { ob[key] = val; return ob }, {})
-			} else {
-				cursor = cursor[cursorName] = Object.defineProperty({}, "valueOf",
-																														{
-					enumerable: false,
-					value: (function(old) { return old; }).bind( null, cursor[cursorName])
-				});
-			}
-		}
-		cursor[valueReceptor] = value;
-
-		return this;
+	value: function(path, val) {
+		if (arguments.length===1) return deepGet.bind(this)(path)
+		else deepSet.bind(this)(path, val)
 	}
-});
+})
 
-Object.defineProperty(Object.prototype, "deepGet", {
-	enumerable: false,
-	value: function (path) {
-		if (!isString(path)) { 
-			throw new Error("Invalid path");
-		}
-		var pathArry = path.split('.');
+function deepSet (path, value) {
+	function isArray(ob) {
+		return Object.prototype.toString.call(ob) == '[object Array]'
+	}
+	function isObject(ob) {
+		return Object.prototype.toString.call(ob) == '[object Object]'
+	}
+	function isFunction(ob) {
+		return Object.prototype.toString.call(ob) == '[object Function]'
+	}
 
-		pathArry.forEach(function(val, key) {
+	if (!isObject(this)) throw new Error('Deep Ob is not an available method of Javascript Primitives')
+
+	if (!isString(path)) { 
+		throw new Error("Invalid path")
+	}
+	var pathArry = path.split('.')
+
+	pathArry.forEach(function(val, key) {
 			if (!val.match(/\w+/)) {
-				throw new Error("Invalid property " + val + " in " + this.toString + " " + pathArry.slice(0,key).join('.'));
+			throw new Error("Invalid property " + val + " in " + this.toString + " " + pathArry.slice(0,key).join('.'))
 			}
-		});
+			});
+	var valueReceptor = pathArry.pop();
 
-		var i,imax, cursor;
-		imax = pathArry.length;
-		cursor = this;
+	var i,imax, cursor
+	imax = pathArry.length
+	cursor = this
 
-		for (i=0;i<imax;i++) {
-			var cursorName = pathArry[i];
+	for (i=0;i<imax;i++) {
+		var cursorName = pathArry[i]
 
-			if (cursor === undefined) {
-				return undefined;
-			}
+		if (cursor[cursorName] === undefined) {
+			cursor = cursor[cursorName] = {};
+		} else if (isObject(cursor[cursorName]) || isFunction(cursor[cursorName])) {
 			cursor = cursor[cursorName];
+		} else if (isArray(cursor[cursorName])) {
+			cursor = cursor[cursorName] = cursor[cursorName].reduce(function(ob, val, key) { ob[key] = val; return ob }, {})
+		} else {
+			cursor = cursor[cursorName] = Object.defineProperty({}, "valueOf",
+				{ enumerable: false, value: (function(old) { return old }).bind( null, cursor[cursorName]) })
 		}
-
-		return cursor;
 	}
-});
+	cursor[valueReceptor] = value
+
+	return this
+}
+
+function deepGet(path) {
+	if (!isString(path)) { 
+		throw new Error("Invalid path");
+	}
+	var pathArry = path.split('.');
+
+	pathArry.forEach(function(val, key) {
+			if (!val.match(/\w+/)) {
+			throw new Error("Invalid property " + val + " in " + this.toString + " " + pathArry.slice(0,key).join('.'));
+			}
+			});
+
+	var i,imax, cursor;
+	imax = pathArry.length;
+	cursor = this;
+
+	for (i=0;i<imax;i++) {
+		var cursorName = pathArry[i];
+
+		if (cursor === undefined) {
+			return undefined;
+		}
+		cursor = cursor[cursorName];
+	}
+
+	return cursor;
+}
--- test/test.js
+++ test/test.js
@@ -4,24 +4,30 @@ require('../index')
 describe("Deep Properties", ()=> {
 	describe("setter", ()=> {
 		it("assigns a deep property", ()=> {
-			var x = ({}).deepSet("here.is.a.property", 5)
+			var x = ({})
+			console.log(x)
+			console.log(x.deepOb("here.is.a.property"))
+			x.deepOb("here.is.a.property", 5)
+			console.log(x)
 			assert.deepEqual(
 				x,
 				{ here: { is: { a: { property: 5 }}}}
 			)
 		})
 		it("does not overwrite an existing property", ()=> {
-			var x = ({ here: { also: 2 }}).deepSet("here.is.a.property", 5)
+			var x = ({ here: { also: 2 }})
+			x.deepOb("here.is.a.property", 5)
 			assert.deepEqual(
 				x,
 				{ here: { also: 2, is: { a: { property: 5 }}}}
 			)
 		})
 		it("preserves an existing array by mapping it to object", ()=> {
-			var x = ([ "other", "values" ]).deepSet("here.is.a.property", 5)
+			var x = ([ "other", "values" ])
+			x.deepOb("here.is.a.property", 5)
 			assert.deepEqual(
 				x,
-				{ "other", "values", here: { also: 2, is: { a: { property: 5 }}}}
+				{ 0:"other", 1:"values", here: { also: 2, is: { a: { property: 5 }}}}
 			)
 		})
 	})
